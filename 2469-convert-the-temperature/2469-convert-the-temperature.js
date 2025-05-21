46423087e752762cfc29d1fce4170b3fb105f271
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-05-21 13:46:04 -0700

    Time: 0 ms (100%), Space: 53.2 MB (77.71%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.js
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -3,9 +3,10 @@
  * @return {number[]}
  */
 var convertTemperature = function(celsius) {
-    let result =[]
+    // let result =[]
 
-    result.push([celsius + 273.15, celsius * 1.80 + 32.00])
-    return result
+    // result.push([celsius + 273.15, celsius * 1.80 + 32.00])
+    // return result
+     return [celsius + 273.15, celsius * 1.80 + 32.00];
     
 };
