714f525c4a8929465a3de765f5aabf4eeadfa66c
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-05-22 19:46:28 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var findTargetSumWays = function(nums, target) {
+    let result = 0; 
+
+    const dfs = (i,nums,target, runningSum)=>{
+        //base case 
+        if(i===nums.length){ //leaf level
+        if(runningSum === target){
+            result ++;
+        }
+        return;
+        }
+        //recursive helper 
+        //include + 
+        runningSum += num[i]
+        dfs(i+1,nums,target,runningSum)
+        runningSum -= num[i]
+        //-
+         runningSum -= num[i]
+        dfs(i+1,nums,target,runningSum)
+        runningSum += num[i]
+    }
+    dfs(0,nums,target,0)
+    return result
+};
