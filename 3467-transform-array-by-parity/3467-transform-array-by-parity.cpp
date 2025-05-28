d61718464c04204c077f48c7e267e866c8c14cf5
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-05-28 10:39:01 -0700

    Time: 4 ms (8.4%), Space: 28.3 MB (25.59%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    vector<int> transformArray(vector<int>& nums) {
-     
-
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            ans.push_back(nums[i]%2);
+    std::vector<int> transformArray(std::vector<int>& nums) {
+        // Transform the array
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 == 0) {
+                nums[i] = 0;         
+            } else {
+                nums[i] = 1;
+            }
         }
-        sort(ans.begin(),ans.end());
-        return ans;
+        // Sort the transformed array
+        std::sort(nums.begin(), nums.end());
+        return nums;
     }
 };
-
