ef87f03c610fa385dbfcdbda28cbc00a198b9939
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-06-13 22:31:27 -0600

    Time: 2 ms (58.87%), Space: 53.7 MB (87.94%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.js
@@ -0,0 +1,14 @@
+var countAsterisks = function(s) {
+    let valid = true;
+    let res = 0;
+    for (let i = 0; i < s.length; i++) {
+        let c = s[i];
+        if (c === '|') {
+            valid = !valid;
+        } else if (c === '*' && valid) {
+            res++;
+        }
+    }
+    return res;
+};
+ 
