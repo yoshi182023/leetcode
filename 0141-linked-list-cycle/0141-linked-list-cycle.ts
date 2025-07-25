9a54700581070308ec16dd2a8ef7a5636f94087c
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-07-25 14:56:06 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+    if(head == null){
+        return false; 
+    }
+    let slow = head; 
+    let fast = head; 
+    while(fast.next !== null && fast.next.next !== null){
+        slow = slow.next
+        fast = fast.next.next
+        if(slow == fast){
+            return ture
+        }
+    }
+    return false
+};
