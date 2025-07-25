768b40ab22a66af7f12ba154fbfeddcdd1675bdf
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-07-25 14:58:09 -0700

    Time: 52 ms (38.58%), Space: 57.8 MB (79.47%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.ts
+++ 0141-linked-list-cycle/0141-linked-list-cycle.ts
@@ -17,6 +17,8 @@ function hasCycle(head: ListNode | null): boolean {
     let slow = head; 
     let fast = head; 
     while(fast.next !== null && fast.next.next !== null){
+        //只要 fast 能继续走两步，就继续循环；否则就停止。
+        //确保 fast 能够再走两步，才进入下一轮循环，从而 避免访问 null 的 next，防止运行时错误。
         slow = slow.next
         fast = fast.next.next
         if(slow == fast){
