ac77bf6a78d71d453a9893e10faab25bb0c30573
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-07-25 00:27:56 -0700

    Time: 5 ms (97.92%), Space: 60.2 MB (14.58%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.ts
+++ 0147-insertion-sort-list/0147-insertion-sort-list.ts
@@ -1,58 +1,48 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
-function insertionSortList(head: ListNode | null): ListNode | null {
-     if (head === null) {
-        return head;
-    }
+ function insertionSortList(head: ListNode | null): ListNode | null {
+      if (head === null) {
+         return head;
+     }
+     //dummyHead(0) → 2 → 1 → 4 → 3 → null
         const dummyHead = new ListNode(0);
-    dummyHead.next = head;//next node is our real head of our linked list
-//handle edge cases 
-//keep tracking of 2 pointers
- let prev = head, curr = head.next;
-     // - curr: current node being inserted
-
-//start checking until we reach the end of the linked list 
- while (curr !== null) {
-    //maybe it is already in order 
-                // Current node is already in correct position
-
-    if(curr.val >= prev.val){
-        //continute to the next iteration of the loop 
-        //update the previous and current pointers
-        prev = curr
-        curr = curr.next
-        continue 
-    }else{            // Need to find the correct position to insert current node
-        //if it's not the case 
-        //we want to start from the beginning of the linked list
-        let temp = dummyHead
-        while( temp.next !== null && curr.val > temp.next.val){
-            temp = temp.next
-        }
-        // Perform the insertion:
-       // 1. Remove current node from its original position
-        prev.next = curr.next
-        curr.next = temp.next 
-        temp.next = curr
-        curr = prev.next
-    }
-  
- }
-
-
-
-
-  return dummyHead.next 
-
-
-
-};
+     dummyHead.next = head;//next node is our real head of our linked list
+ //handle edge cases 
+ //keep tracking of 2 pointers
+  let prev = head, curr = head.next;
+// - curr: current node being inserted
+//  prev: 2 (head)2 → 1 → 4 → 3 
+// curr: 1 (head.next)2 → 1 → 4 → 3 → null
+ //start checking until we reach the end of the linked list 
+  while (curr !== null) {
+     //maybe it is already in order 
+     // Current node is already in correct position
+     if(curr.val >= prev.val){
+         //continute to the next iteration of the loop 
+         //update the previous and current pointers
+         prev = curr
+         curr = curr.next
+         continue 
+     }else{         // Need to find the correct position to insert current node
+         //if it's not the case 
+         //we want to start from the beginning of the linked list
+         let temp = dummyHead //dummyHead(0) 
+         while( temp.next !== null && curr.val > temp.next.val){ // temp.next.val = dummyHead(0) → 2
+             temp = temp.next
+         }
+         // Perform the insertion:
+        // 1. Remove current node from its original position
+         prev.next = curr.next //2 → 2.next = 4
+         curr.next = temp.next //1.next = 2
+        //  dummyHead(0) → 2 → 1 → 4 → 3 → null
+        //    ↑                ↑
+        //   temp             curr
+        //   (dummyHead) (值为1的节点)
+         temp.next = curr  //即：dummyHead.next = 1
+         curr = prev.next
+     }
+   
+  }
+ 
+   return dummyHead.next 
+ 
+ 
+ };
