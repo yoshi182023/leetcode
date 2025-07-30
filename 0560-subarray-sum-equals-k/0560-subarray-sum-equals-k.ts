6043dd8ee6685fa31e46f39f1b270f7c4484af18
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-07-29 22:46:09 -0700

    Time: 21 ms (41.06%), Space: 66.1 MB (25.7%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.ts
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.ts
@@ -12,7 +12,7 @@ function subarraySum(nums: number[], k: number): number {
     for(let n of nums){
         currSum += n
         //更新前缀和。此时 currSum 表示从开头到当前元素的和。
-        diff = currSum - k//我们要找的是：有没有某个之前的前缀和 prevSum，使得
+        let diff = currSum - k//我们要找的是：有没有某个之前的前缀和 prevSum，使得
         if(prefixSum.has(diff)){
             //如果之前出现过这个差值（currSum - k），那么就说明存在一个或多个连续子数组，它们的和等于 k，我们就把这些情况加到结果中。
         res += prefixSum.get(diff)
