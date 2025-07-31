9583a962adc0ad70f875cdbeb2bbf6109037ea6a
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-07-31 14:53:51 -0700

    Time: 18 ms (28%), Space: 59 MB (68.8%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -16,6 +16,8 @@ var topKFrequent = function(nums, k) {
     }
     //console.log(map)//{ '1': 3, '2': 2, '3': 1 }
     for(let [num,freq] of Object.entries(map)){//用来把对象的每个键值对转换成数组形式的元素
+            num = Number(num);  // 这里转换为数字
+
         //console.log(Object.entries(map))[ ["1", 3], ["2", 2], ["3", 1] ]
         if(!bucket[freq]){
             bucket[freq] = new Set().add(num)// bucket[freq] = new Set().add(num)，它的返回值是 Set 本身（因为 add() 方法返回该 Set），所以是 OK 的。
@@ -35,6 +37,7 @@ var topKFrequent = function(nums, k) {
         if(bucket[i]) result.push(...bucket[i])
         if(result.length === k) break
     }
+    console.log(result)
     return result
 };
 
