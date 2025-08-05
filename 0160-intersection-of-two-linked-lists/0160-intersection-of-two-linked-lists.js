d042595ea4793eab15b4ce4d6787faadee63d50c
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-08-04 18:24:57 -0700

    Time: 51 ms (84.8%), Space: 63.8 MB (63.7%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -12,11 +12,25 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    let hA = headA;
-    let hB = headB; 
+    let hA = headA;//链表A的头节点
+    let hB = headB; //链表B的头节点
     while(hA !== hB){
-        hA = (hA === null) ? headB : hA.next
+
+if (hA === null) {
+    hA = headB;  // 如果 hA 走到链表A的末尾，就跳转到链表B的头节点
+} else {
+    hA = hA.next; // 否则，继续向后移动一步
+}
+        //如果 hA 走到链表A的末尾（null），就让它从 headB 重新开始。
         hB = (hB === null) ? headA : hB.next
     }
     return hA;
 };
+
+
+
+
+
+
+
+
