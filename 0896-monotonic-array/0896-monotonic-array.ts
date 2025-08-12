e675b82a8039bb31203c773ad894f365d1c32524
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-08-12 00:04:04 -0700

    Time: 4 ms (33.33%), Space: 67.9 MB (28.21%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.ts
@@ -0,0 +1,16 @@
+function isMonotonic(nums: number[]): boolean {
+    let decrease = true 
+    let increase = true;
+  
+    let n = nums.length -1 
+
+    for(let i = 0; i < n; i++){
+        if (!(nums[i] <= nums[i+1])){ //increasing 
+            increase = false 
+        }
+         if (!(nums[i] >= nums[i+1])){ //increasing 
+            decrease = false 
+        }
+    }
+        return increase || decrease 
+};
