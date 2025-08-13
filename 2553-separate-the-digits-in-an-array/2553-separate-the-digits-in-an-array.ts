f01bfa22bf0725773bd2d9f1222a8601a5ecc5ab
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-08-12 20:01:16 -0700

    Time: 430 ms (5.88%), Space: 65.9 MB (5.88%) - LeetHub

---
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.ts
@@ -0,0 +1,27 @@
+function separateDigits(nums: number[]): number[] {
+  
+ 
+    let result = []
+    for(let num of nums ){
+        while(num > 0 ){
+           const digits = [];
+           if (num === 0) {
+            digits.push(0);
+        } else {
+            while (num > 0) {
+                digits.push(num % 10);
+                console.log("num",num)
+                console.log(digits)
+                num = Math.floor(num / 10);
+                console.log("math nun",num)
+            }
+        }
+        // Push digits in correct order (reverse of how we collected them)
+        console.log("digitsloop",digits)
+        for (let i = digits.length - 1; i >= 0; i--) {
+            result.push(digits[i]);
+        }
+    }
+    }
+    return result;
+};
