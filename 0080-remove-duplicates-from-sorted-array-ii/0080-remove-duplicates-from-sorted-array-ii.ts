31c819802a3e41d77269730558cd669d09897d5e
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-08-16 19:29:01 -0700

    Time: 56 ms (63.5%), Space: 60 MB (18.41%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.ts
@@ -0,0 +1,11 @@
+function removeDuplicates(nums: number[]): number {
+    
+    let i = 0; 
+    for(let currentNum of nums){
+        if(i < 2 || currentNum !== nums[i-2]){
+            nums[i] = currentNum
+            i++
+        }
+    }
+    return i
+};
