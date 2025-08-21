08357c35ccf39891ffe444d034b91e0ae3af5612
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-08-21 01:01:31 -0700

    Time: 1092 ms (5.21%), Space: 77.5 MB (5.21%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.ts
+++ 0901-online-stock-span/0901-online-stock-span.ts
@@ -1,11 +1,16 @@
-class StockSpanner {
-    constructor() {
-            this.stack = []// 栈存 [price, span]
 
-    }
 
-    next(price: number): number {
-         let span = 1
+
+var StockSpanner = function() {
+    this.stack = []// 栈存 [price, span]
+};
+
+/** 
+ * @param {number} price
+ * @return {number}
+ */
+StockSpanner.prototype.next = function(price) {
+    let span = 1
     while(this.stack.length > 0  && this.stack[this.stack.length-1][0] <= price){
         span += this.stack[this.stack.length-1][1]
         this.stack.pop()
@@ -18,36 +23,10 @@ class StockSpanner {
     console.log("re",span)
     return span
 
-    }
-}
+};
 
-
-// var StockSpanner = function() {
-//     this.stack = []// 栈存 [price, span]
-// };
-
-// /** 
-//  * @param {number} price
-//  * @return {number}
-//  */
-// StockSpanner.prototype.next = function(price) {
-//     let span = 1
-//     while(this.stack.length > 0  && this.stack[this.stack.length-1][0] <= price){
-//         span += this.stack[this.stack.length-1][1]
-//         this.stack.pop()
-//     }
-//      // 把当前价格和跨度入栈
-//     this.stack.push([price, span])
-//     console.log(this.stack)
-
-//     // 返回跨度
-//     console.log("re",span)
-//     return span
-
-// };
-
-// /** 
-//  * Your StockSpanner object will be instantiated and called as such:
-//  * var obj = new StockSpanner()
-//  * var param_1 = obj.next(price)
-//  */
+/** 
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */
