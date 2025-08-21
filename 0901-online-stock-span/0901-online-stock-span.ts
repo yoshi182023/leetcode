3a1f9bc482dcb161aca2d72cd8bf3ae57cbb9e76
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-08-21 01:01:28 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.ts
@@ -0,0 +1,53 @@
+class StockSpanner {
+    constructor() {
+            this.stack = []// 栈存 [price, span]
+
+    }
+
+    next(price: number): number {
+         let span = 1
+    while(this.stack.length > 0  && this.stack[this.stack.length-1][0] <= price){
+        span += this.stack[this.stack.length-1][1]
+        this.stack.pop()
+    }
+     // 把当前价格和跨度入栈
+    this.stack.push([price, span])
+    console.log(this.stack)
+
+    // 返回跨度
+    console.log("re",span)
+    return span
+
+    }
+}
+
+
+// var StockSpanner = function() {
+//     this.stack = []// 栈存 [price, span]
+// };
+
+// /** 
+//  * @param {number} price
+//  * @return {number}
+//  */
+// StockSpanner.prototype.next = function(price) {
+//     let span = 1
+//     while(this.stack.length > 0  && this.stack[this.stack.length-1][0] <= price){
+//         span += this.stack[this.stack.length-1][1]
+//         this.stack.pop()
+//     }
+//      // 把当前价格和跨度入栈
+//     this.stack.push([price, span])
+//     console.log(this.stack)
+
+//     // 返回跨度
+//     console.log("re",span)
+//     return span
+
+// };
+
+// /** 
+//  * Your StockSpanner object will be instantiated and called as such:
+//  * var obj = new StockSpanner()
+//  * var param_1 = obj.next(price)
+//  */
