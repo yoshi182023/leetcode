01d820ca9f5b50f13f46c9f09449b02082cbb70e
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-08-31 00:22:19 -0700

    Time: 25 ms (2.19%), Space: 58.6 MB (5.85%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.js
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.js
@@ -1,10 +1,4 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+
 /**
  * @param {ListNode} head
  * @param {number} left
@@ -20,11 +14,17 @@ var reverseBetween = function(head, left, right) {
         left--;
         right--
     }
+// 反转区间的长度是 right - left + 1。
+// 当我们把 curr 移动到第 left 个节点时，已经往前走了 (left-1) 步。
+// 这时候，如果还保持原来的 right，那么反转的长度会变大。
+// 所以要同时 right--，保持区间长度不变。
     let start = prev 
     let tail = curr
     let temp = null 
     while(right > 0){
+        console.log("right",right)
         temp = curr.next 
+         console.log("temp",temp)
         curr.next = prev
         prev = curr
         curr = temp 
