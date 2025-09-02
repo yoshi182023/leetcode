0534cb9bce8e52b829c853d3fb5244a184eb516d
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-09-01 18:35:29 -0700

    Time: 5 ms (11.83%), Space: 61.1 MB (16.78%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -7,6 +7,14 @@
  * }
  */
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
  * @param {TreeNode} root
  * @return {boolean}
  */
@@ -16,9 +24,9 @@ var isBalanced = function(root) {
         if(root === null) return 0
         return 1+ Math.max(helper(root.left),helper(root.right))
     }
-
     if(Math.abs ( helper(root.left)- helper(root.right) ) <2 ){
-         return isBalanced(root.left) && isBalanced(root.right)
+         return isBalanced(root.left) && isBalanced(root.right) 
+         //这样不仅判断根节点，还会判断每个子树。
     }
     
      return false
