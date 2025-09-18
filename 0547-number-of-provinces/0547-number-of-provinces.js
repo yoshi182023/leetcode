d66e43c4c8047ff427a03be5e4eea89d0db09648
Yoshi Gong <125266206+yoshi182023@users.noreply.github.com>
2025-09-17 19:08:12 -0700

    Time: 42 ms (5.03%), Space: 62.1 MB (6.28%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+var findCircleNum = function(isConnected) {
+    let adj = {}; 
+    for(let i =0; i< isConnected.length; i++){
+        for(j=0; j< isConnected[0].length; j++){
+            let val = isConnected[i][j]
+            if(val === 1){
+                if(!adj[i]){
+                    adj[i] =[j]
+                    console.log(adj[i])
+                }else{
+                    adj[i].push(j)
+        
+                }
+// i=0, j=0 → val=1，还没有 adj[0]，所以 adj[0] = [0]
+// i=0, j=1 → val=1，此时已有 adj[0]，所以 adj[0].push(1) → adj[0] = [0,1]
+// i=1, j=0 → val=1，新建 adj[1] = [0]
+// i=1, j=1 → val=1，adj[1].push(1) → adj[1] = [0,1]
+// i=2, j=2 → val=1，新建 adj[2] = [2]
+            }
+        }
+    }
+    let visited = new Set()
+    let count = 0; 
+    for (let key in adj){
+        //the key in any object is always going to be a string 
+        let keyNum = parseInt(key);
+        count += dfs(keyNum)
+    }
+
+    function dfs(currNode){
+        if(visited.has(currNode)){
+            return 0
+        }
+        visited.add(currNode)
+        let neighbours = adj[currNode]
+
+        for(let n of neighbours){
+            dfs(n)
+        }
+        return 1
+    }
+    return count
+};
