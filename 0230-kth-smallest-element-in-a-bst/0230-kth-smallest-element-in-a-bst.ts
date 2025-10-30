4dc77191ef1ef652a41667efd1f65d70039b3c94
Mia Gong <125266206+yoshi182023@users.noreply.github.com>
2025-10-30 16:07:07 -0700

    Time: 348 ms (5.14%), Space: 73.7 MB (5.57%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.ts
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function kthSmallest(root: TreeNode | null, k: number): number {
+    /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+ 
+    let n = 0
+    let stack = []
+    //iteratively 
+    let cur = root //tell us what node we are currently visiting 
+ while (cur !== null || stack.length > 0) {
+        //traversing our binary tree 
+        //while cur is not null
+      while (cur !== null) {
+    stack.push(cur)
+            //keep going left 
+            //go through every node in the left subtree before visit the current node 
+            console.log("stack",stack)
+            
+            cur = cur.left 
+        }
+        cur = stack.pop()
+    
+        n +=1 
+        if (n === k){
+            return cur.val
+        }
+            cur= cur.right
+        }
+            return -1; // 如果k超出范围
+    
+};
