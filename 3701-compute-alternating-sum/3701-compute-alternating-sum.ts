0fe072b0b8050e6b9806f2a698565ec5b2221e07
Mia Gong <125266206+yoshi182023@users.noreply.github.com>
2025-12-15 17:24:03 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.ts
@@ -0,0 +1,12 @@
+function alternatingSum(nums: number[]): number {
+    var alternatingSum = function(nums) {
+    let result = 0
+    for(let i = 0; i < nums.length; i++){
+        if ( i % 2 === 0){
+            result += nums[i]
+        }else{
+            result -= nums[i]
+        }
+    }
+    return result 
+};
