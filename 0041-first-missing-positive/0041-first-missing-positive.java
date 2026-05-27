eda621e3697cdfeffaf60f3e35cca1a727ddefff
Gong <125266206+yoshi182023@users.noreply.github.com>
2026-05-26 20:38:35 -0400

    Time: 1 ms (100%), Space: 71.2 MB (56.66%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -5,10 +5,14 @@
 // }
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
+  
+
+     int n = nums.length;
 
         for (int i = 0; i < n; i++) {
+            //只要当前元素在1-n的范围内，但它不在正确的位置上
             while (nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                //避免出现重复的元素
                 int correctIndex = nums[i] - 1;
                 int temp = nums[i];
                 nums[i] = nums[correctIndex];
